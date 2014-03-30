1804f1c5dfe6278569c42c35a51c905764703b62
GitMyCode <marbouchl@gmail.com>
2014-03-30 15:00:01 -0400

    ajout de commentaire

--- src/Controllers/GameController.java
+++ src/Controllers/GameController.java
@@ -134,7 +134,7 @@ public class GameController implements MouseListener, ActionListener{
 
         }else if(e.getActionCommand() == "Configurer"){
             System.out.println("Configurer");
-            syncOptionAndConfiguration();
+            syncFromOption();
             vConf.setVisible(true);
         }
 
@@ -150,28 +150,36 @@ public class GameController implements MouseListener, ActionListener{
 
     }
 
-    public void syncOptionAndConfiguration(){
+    /*
+    * Va chercher les données dans Options et synchronise la vue Configuration
+    * sur ces données
+    * */
+    public void syncFromOption(){
 
 
+        // Pour le type du joueur 1 : AI ou Humain
         switch (mOptions.getPlayer1Type()){
             case AI:     vConf.getGroupP1type().setSelected(vConf.getP1Ai().getModel(),true); break;
             case HUMAN : vConf.getGroupP1type().setSelected(vConf.getP1Human().getModel(),true); break;
         }
 
+        // Pour le type du joueur 2 : Ai ou Humain
         switch (mOptions.getPlayer2Type()){
             case AI:     vConf.getGroupP2type().setSelected(vConf.getP2Ai().getModel(),true); break;
             case HUMAN : vConf.getGroupP2type().setSelected(vConf.getP2Human().getModel(),true); break;
         }
 
-
+        // Pour le joueur qui commence la partie
         switch (mOptions.getStartingPlayer()){
             case 1: vConf.getGroupTurn().setSelected(vConf.getP1Turn().getModel(),true); break;
             case 2: vConf.getGroupTurn().setSelected(vConf.getP2Turn().getModel(),true); break;
         }
 
+        // La taille de la grille
         vConf.getRow().setText(String.valueOf(mOptions.getSizeX()));
         vConf.getCol().setText(String.valueOf(mOptions.getSizeY()));
 
+        // Si on peut clicker au dessu ou doit etre exact
         if(mOptions.isAllowClickAbove()){
             vConf.getGroupPlacement().setSelected(vConf.getCoulisse().getModel(),true);
         }else{

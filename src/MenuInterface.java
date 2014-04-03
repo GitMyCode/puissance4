bb3a9964025baae6a911ed598ef39f3de59c03ea
GitMyCode <marbouchl@gmail.com>
2014-04-03 17:41:00 -0400

    commit version_1

---
+++ src/MenuInterface.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Martin on 4/2/2014.
+ */
+public interface MenuInterface {
+}
---
+++ src/Models/GLOBAL.java
@@ -0,0 +1,16 @@
+package Models;
+
+/**
+ * Created by Martin on 4/2/14.
+ */
+public enum GLOBAL {
+
+    AI,
+    HUMAN,
+    IN_GAME,
+    GAME_NULL,
+
+
+
+
+}
